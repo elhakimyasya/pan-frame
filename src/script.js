2a5d41ed6bd77dabb1f8509e4f09c76da49d85aa
Yasya El Hakim <yasyaelhakim@gmail.com>
2023-02-24 17:35:20 +0700

    update scripts.min.js and script.js

--- src/script.js
+++ src/script.js
@@ -4,17 +4,24 @@ import './styles.css';
 
 const resizeText = (selector) => {
     const textEl = document.querySelector(selector);
-    const maxHeight = textEl.parentNode.offsetHeight;
+    const containerEl = textEl.parentNode;
+    let maxHeight = containerEl.offsetHeight;
+
+    const observer = new ResizeObserver(() => {
+        const newMaxHeight = containerEl.offsetHeight;
+        if (maxHeight !== newMaxHeight) {
+            maxHeight = newMaxHeight;
+            onMaxHeightChange();
+        }
+    });
+    observer.observe(containerEl);
+
     let textHeight = textEl.offsetHeight;
     const ratio = maxHeight / textHeight;
     const fontSize = Math.min(100, ratio * 100);
-
     if (maxHeight < textHeight) {
         textEl.style.fontSize = fontSize + '%';
         textEl.style.lineHeight = 1.3 * fontSize / 100;
-    } else {
-        textEl.style.fontSize = '';
-        textEl.style.lineHeight = '';
     }
 };
 
@@ -29,6 +36,10 @@ const imageToBase64 = (file) => new Promise((resolve, reject) => {
         reject(error);
     };
 });
+
+const isMobile = () => {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+};
 
 elcreativeConfig.options.optionFeatureImageZoom = "1px";
 
@@ -180,12 +191,22 @@ if (navigator.share) {
     });
 } else {
     buttonShareAlt.style.display = 'none';
-}
+};
 
 resizeText('#post_body .content_primary .__wrapper');
 resizeText('#post_body .content_secondary .__wrapper');
 
+
 window.addEventListener('resize', () => {
-    resizeText('#post_body .content_primary .__wrapper');
-    resizeText('#post_body .content_secondary .__wrapper');
+    if (!isMobile()) {
+        resizeText('#post_body .content_primary .__wrapper');
+        resizeText('#post_body .content_secondary .__wrapper');
+    }
+});
+
+window.addEventListener('orientationchange', () => {
+    if (!isMobile()) {
+        resizeText('#post_body .content_primary .__wrapper');
+        resizeText('#post_body .content_secondary .__wrapper');
+    }
 });
